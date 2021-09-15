faa47ff9dc2cac70fb28467e6dda7b97830aa444
Eric Keen <eric.k@hyfe.ai>
2021-09-15 11:32:03 -0500

    added demo code

---
+++ R/demo.R
@@ -0,0 +1,39 @@
+################################################################################
+################################################################################
+# Demo of using resources in 'iep' repo
+################################################################################
+################################################################################
+
+# Load libraries
+library(rstudioapi)
+library(readr)
+library(dplyr)
+
+# Set wd
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+# Source functions from other R code
+source('functions.R')
+
+################################################################################
+# Load data
+
+plastics <- read_csv('https://raw.githubusercontent.com/ericmkeen/iep/main/data/plastics-2021.csv')
+head(plastics)
+
+transects <- read_csv('https://raw.githubusercontent.com/ericmkeen/iep/main/data/transects-2021.csv')
+head(transects)
+
+################################################################################
+# Map data
+
+map_stcats()
+points(transects$longitude,
+       transects$latitude,
+       col='red')
+
+
+
+
+
+
